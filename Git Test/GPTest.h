395cd9cb83a147d532a8d0f9e757eb973ca1fdb3
AppDev <appdev@curt-criders-macbook-pro.local>
2014-04-10 15:28:39 -0500

    added a test class

---
+++ Git Test/GPTest.h
@@ -0,0 +1,13 @@
+//
+//  GPTest.h
+//  Git Test
+//
+//  Created by AppDev on 4/10/14.
+//  Copyright (c) 2014 Grasta Productions, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPTest : NSObject
+
+@end
---
+++ Git Test/GPTest.m
@@ -0,0 +1,13 @@
+//
+//  GPTest.m
+//  Git Test
+//
+//  Created by AppDev on 4/10/14.
+//  Copyright (c) 2014 Grasta Productions, Inc. All rights reserved.
+//
+
+#import "GPTest.h"
+
+@implementation GPTest
+
+@end
